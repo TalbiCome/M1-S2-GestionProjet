633e2e34ef859144221322be86070d8801b05d41
Côme Talbi <117739015+TalbiCome@users.noreply.github.com>
2024-06-03 17:07:40 +0200

    dockerCompose Pom version update Fix of worker crashing because of connection error at launch Readme for project Setup

---
+++ Readme.md
@@ -0,0 +1,4 @@
+### Project Setup
+
+docker build --pull --rm -f "./Dockerfile" -t gestiondeprojet-loginapp:latest "."  
+docker-compose up --build  
--- src/main/java/demo/controller/WorkerController.java
+++ src/main/java/demo/controller/WorkerController.java
@@ -1,6 +1,9 @@
 package demo.controller;
 
 import demo.model.Worker;
+
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpStatus;
@@ -17,6 +20,7 @@ public class WorkerController {
 
     @EventListener(ApplicationReadyEvent.class)
     public void afterStartup(){
+      try{
         this.hostname = System.getenv().get("HOSTNAME");
         if (this.hostname!= null){
             this.self = new Worker(hostname);
@@ -26,6 +30,15 @@ public class WorkerController {
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(this.self).retrieve();
         }
+      }catch (Exception e){
+        try {
+          TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e1) {
+          e1.printStackTrace();
+        }
+        afterStartup();
+      }
+
     }
     @GetMapping("/hello2")
     public ResponseEntity<String> hello(){
